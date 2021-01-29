56d598b69aa18b296a7b8cc68d4eacd24480cb43
ruanpiano <ruanmarins96@gmail.com>
2021-01-29 14:02:08 -0300

    - Reidentação de código

--- js/main.js
+++ js/main.js
@@ -1,69 +1,63 @@
 d3.csv("https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv")
-.then(makeChart);
+    .then(makeChart);
 
 function makeChart(data) {
-    if (window.innerHeight <= 800 || window.devicePixelRatio>=2) {
-        var altura = window.innerHeight - window.innerHeight*0.3;
+    if (window.innerHeight <= 800 || window.devicePixelRatio >= 2) {
+        var altura = window.innerHeight - window.innerHeight * 0.3;
         document.getElementById('vacinasPorEstado').height = altura;
         document.getElementById('totalVacinas').height = altura;
     }
-    var generalData = data.map(function(d) {return [d.date, d.vaccinated, d.state];});    
+    var generalData = data.map(function(d) { return [d.date, d.vaccinated, d.state]; });
     var usefulData = [];
-    var stateData = [];    
-    var latestData = generalData[generalData.length-1][0];    
-    
+    var stateData = [];
+    var latestData = generalData[generalData.length - 1][0];
+
     generalData.forEach(function(row, i) {
-        if ((parseInt(row[1]) > 0) && (row[1] != "")) 
-        {
+        if ((parseInt(row[1]) > 0) && (row[1] != "")) {
             row[1] = parseInt(row[1]);
-            if (row[2] == "TOTAL")
-            {            
+            if (row[2] == "TOTAL") {
                 var dataVar = new Date(row[0]);
-                var dataFormatada = dataVar.getDate()+1+'/'+(dataVar.getUTCMonth()+1)+'/'+dataVar.getFullYear();            
-                row[0] = dataFormatada;                
-                usefulData.push(row);            
-            }
-            else
-            {
+                var dataFormatada = dataVar.getDate() + 1 + '/' + (dataVar.getUTCMonth() + 1) + '/' + dataVar.getFullYear();
+                row[0] = dataFormatada;
+                usefulData.push(row);
+            } else {
                 if (row[0] == latestData)
-                stateData.push(row);
+                    stateData.push(row);
             }
-        } 
-    });    
+        }
+    });
 
-    var datasUnicas = usefulData.map(function(d) {return d[0];});
+    var datasUnicas = usefulData.map(function(d) { return d[0]; });
 
-    var dadosVacinas = usefulData.map(function(d) {return d[1];});
+    var dadosVacinas = usefulData.map(function(d) { return d[1]; });
 
-    var ultimas24Horas = dadosVacinas[dadosVacinas.length-1]-dadosVacinas[dadosVacinas.length-2];
+    var ultimas24Horas = dadosVacinas[dadosVacinas.length - 1] - dadosVacinas[dadosVacinas.length - 2];
 
     document.getElementById('24horas').innerText = "+" + ultimas24Horas + " pessoas vacinadas.";
 
-    
 
-    stateData.sort(function (a, b) {
-        if (a[1] > b[1]) {            
-          return -1;
+
+    stateData.sort(function(a, b) {
+        if (a[1] > b[1]) {
+            return -1;
         }
         if (a[1] < b[1]) {
-          return 1;
-        }        
+            return 1;
+        }
         return 0;
-      });    
-    
+    });
+
     var totalVacinas = new Chart(document.getElementById('totalVacinas').getContext('2d'), {
-        type: 'line',    
+        type: 'line',
         data: {
             labels: datasUnicas,
-            datasets: [
-                {
-                    label: "Quantidade de pessoas vacinadas no país",
-                    backgroundColor: "#0000FF55",
-                    data: dadosVacinas,
-                }
-            ],
+            datasets: [{
+                label: "Quantidade de pessoas vacinadas no país",
+                backgroundColor: "#0000FF55",
+                data: dadosVacinas,
+            }],
         },
-        options: {         
+        options: {
             legend: {
                 display: false
             },
@@ -81,21 +75,19 @@ function makeChart(data) {
     });
 
     var vacinasPorEstado = new Chart(document.getElementById('vacinasPorEstado').getContext('2d'), {
-        type: 'horizontalBar',    
+        type: 'horizontalBar',
         data: {
-            labels: stateData.map(function(d) {return d[2];}),
-            datasets: [
-                {
-                    label: "Quantidade de pessoas vacinadas por estado",
-                    backgroundColor: "#0000FF55",
-                    data: stateData.map(function(d) {return d[1];})                                    
-                }
-            ],
-        },      
-        options: {       
+            labels: stateData.map(function(d) { return d[2]; }),
+            datasets: [{
+                label: "Quantidade de pessoas vacinadas por estado",
+                backgroundColor: "#0000FF55",
+                data: stateData.map(function(d) { return d[1]; })
+            }],
+        },
+        options: {
             legend: {
                 display: false
-            },     
+            },
             scales: {
                 xAxes: [{
                     ticks: {
@@ -106,6 +98,6 @@ function makeChart(data) {
                     }
                 }]
             }
-        } 
+        }
     });
 }
